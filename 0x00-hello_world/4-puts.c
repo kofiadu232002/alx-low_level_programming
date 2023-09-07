dcca49c2f13f5c1717057737af811551a41c932a
Kofi_Adu <kofiadu232002@gmail.com>
2023-09-07 20:37:53 +0000

    the rest

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * * main - prints the string in the put function
+ * * description -this programm prints "programming is like building a multilingual puzzle"
+ * * return: 0
+ * */
+
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+/**
+ * *
+ * * Entry point of the program
+ * *
+ * * Return: Always 0
+ * */
+
+int main(void)
+{
+		printf("with proper grammar, but thr outcome is a piece of art,\n");
+			return (0);
+}
