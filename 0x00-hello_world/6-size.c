6983e0a37c19a31ca4e7061aa7d053d6c8ce534f
Kofi_Adu <kofiadu232002@gmail.com>
2023-09-07 21:58:12 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,19 @@
-
-
 #include <stdio.h>
 /**
- * * main - prints a string
- * * description - this uses the main function
- * * Return : 0
- * */
-
+ * main - prints a string
+ * Return : 0
+ */
 int main(void)
-{	
-		char c;
-			int i;
-				long li;
-					long long lli;
-						float f;
-							
-							printf("size of char: %ld byte(s)\n", sizeof(c));
-								printf("size of an int: %ld byte(s)\n", sizeof(i));
-									printf("size of a long int: %ld byte(s)\n", sizeof(li));
-										printf("size of a long long init: %ld byte(s)\n", sizeof(lli));
-											printf("size of a float: %ld byte(s)\n", sizeof(f));
-												return (0);
+{
+char c;
+int i;
+long li;
+long long lli;
+float f;
+printf("size of char: %ld byte(s)\n", sizeof(c));
+printf("size of an int: %ld byte(s)\n", sizeof(i));
+printf("size of a long int: %ld byte(s)\n", sizeof(li));
+printf("size of a long long init: %ld byte(s)\n", sizeof(lli));
+printf("size of a float: %ld byte(s)\n", sizeof(f));
+return (0);
 }
