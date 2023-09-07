722645d300cbb2866f137c48f5b37a9335b13979
Kofi_Adu <kofiadu232002@gmail.com>
2023-09-07 21:23:29 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
-# include <stdio.h>
-
+#include <stdio.h>
 /**
- * *
- * * Entry point of the program
- * *
- * * Return: Always 0
- * */
-
+ * Entry point of the program
+ * Return: Always 0
+ */
 int main(void)
 {
-		printf("with proper grammar, but thr outcome is a piece of art,\n");
-			return (0);
+printf("with proper grammar, but thr outcome is a piece of art,\n");
+return (0);
 }
