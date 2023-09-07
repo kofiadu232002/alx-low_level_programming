bf8b8e6088f510b2bdbeda6c051620ee95858e1e
Kofi_Adu <kofiadu232002@gmail.com>
2023-09-07 21:11:50 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * * main - prints the string in the put function
- * * description -this programm prints "programming is like building a multilingual puzzle"
- * * return: 0
- * */
-
+ * main - A program that prints a line with puts function
+ * Return: 0
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return(0);
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
