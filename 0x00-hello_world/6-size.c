24bbbb9ffb1201dda0c6c79bf1ae9159b20294bc
Kofi_Adu <kofiadu232002@gmail.com>
2023-09-08 03:36:36 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- * main - should write the command
- * Return : 0
+ * main - Entry point
+ *
+ * Return: Always 0
  */
-int main(void)
+int main(void
 {
 char c;
 int i;
-long li;
-long long lli;
+long int li;
+long long int lli;
 float f;
-printf("size of char: %ld byte(s)\n", sizeof(c));
-printf("size of an int: %ld byte(s)\n", sizeof(i));
-printf("size of a long int: %ld byte(s)\n", sizeof(li));
-printf("size of a long long init: %ld byte(s)\n", sizeof(lli));
-printf("size of a float: %ld byte(s)\n", sizeof(f));
+printf("Size of a char: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", sizeof(li));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
 return (0);
-}
