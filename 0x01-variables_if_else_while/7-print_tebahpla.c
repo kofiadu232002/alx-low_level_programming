a1e1123bfeed28b0a24be5a2bfa5e4fac028be37
Kofi_Adu <kofiadu232002@gmail.com>
2023-09-09 03:00:12 +0000

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - Prints all single digit numbers of base 10 starting from 0,
- * only using putchar and without char variables.*
- * Return: Always 0.
+ * main - Entry point
+ * Return: Always 0
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-putchar((num % 10) + '0');
+char str[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 25; i > -1; i--)
+putchar(str[i]);
 putchar('\n');
 return (0);
 }
